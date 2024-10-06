b2fe13a9501294da723a13d040ef9d7972ede619
Artem Tarasov <artemdrolllted@gmail.com>
2024-10-06 20:04:55 +0300

    1111

--- MusListen/Modules/SignInModule/SignInViewController.swift
+++ MusListen/Modules/SignInModule/SignInViewController.swift
@@ -14,6 +14,7 @@ final class SignInViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
+        print("231")
     }
     
 }
